3f86de6800c558ca84bddc8d46b764a80fc62e67
Alex <49212602+prikhodkoas@users.noreply.github.com>
2022-02-01 13:26:04 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -38,7 +38,48 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let resArr = [];
+    let tempExpr = [...expr];
+
+    while(tempExpr.length > 0) {
+        let cur = [];
+
+        for(let i = 0; i < 10; i++) {
+            let temp = tempExpr.shift();
+
+            if(temp === '*') {
+                cur.push(temp);
+                continue;
+            }
+
+            if(cur.length === 0 && temp === '0') {
+                continue;
+            }
+            else {
+                cur.push(temp);
+            }
+        }
+
+        if(cur[0] === '*') {
+            resArr.push(' ');
+        }
+        else {
+            let morzeArr = [];
+
+            for(let i = 1; i < cur.length; i += 2) {
+                if(cur[i] === '0') {
+                    morzeArr.push('.');
+                }
+                else {
+                    morzeArr.push('-');
+                }
+            }
+
+            resArr.push(MORSE_TABLE[morzeArr.join('')]);
+        }
+    }
+
+    return resArr.join('');
 }
 
 module.exports = {
